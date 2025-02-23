b6e68307cdfbfaad49e92781b1691eee71d6037e
hanjx <krismile_code@163.com>
2025-02-23 10:22:06 +0800

    feat: 测试

--- src/test/java/com/qfxl/hotcache/HotCacheApplicationTests.java
+++ src/test/java/com/qfxl/hotcache/HotCacheApplicationTests.java
@@ -1,13 +1,33 @@
 package com.qfxl.hotcache;
 
+import com.alibaba.fastjson2.JSON;
+import com.qfxl.hotcache.cache.IHotCache;
+import com.qfxl.hotcache.model.AreaProvinceVO;
+import com.qfxl.hotcache.model.DictVO;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class HotCacheApplicationTests {
 
+    @Resource(name = "areaCache")
+    private IHotCache<String, AreaProvinceVO> areaCache;
+
+    @Resource(name = "dictCache")
+    private IHotCache<String, DictVO> dictCache;
+
     @Test
     void contextLoads() {
+        String provinceId = "110000";
+        AreaProvinceVO areaProvinceVO = areaCache.get(provinceId);
+        log.info("测试结果: {}", JSON.toJSONString(areaProvinceVO));
+
+        String dictType = "sys_user_sex";
+        DictVO dictVO = dictCache.get(dictType);
+        log.info("测试结果: {}", JSON.toJSONString(dictVO));
     }
 
 }
